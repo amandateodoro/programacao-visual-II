4aab2c5f6f64b137c4d4e98d3915b2053c5f0095
Amanda Teodoro <amandateodoro1408@gmail.com>
2025-04-08 20:11:10 -0400

    aula-03

---
+++ aula-03/AppPessoa/Contexto/ContextBD.cs
@@ -0,0 +1,13 @@
+﻿using AppPessoa.Models;
+
+namespace AppPessoa.Contexto
+{
+    public class ContextBD
+    {
+        public ContextBD()
+        {
+            Pessoas = new List<Pessoa>();
+        }
+        public List<Pessoa>? Pessoas { get; set; }
+    }
+}
