163171f3d157a39827ee1ee639da894450890d76
Amanda Teodoro <amandateodoro1408@gmail.com>
2025-05-06 21:05:31 -0400

    aula-07

---
+++ aula-07/AppMultas/Contexto/ContextoBD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using AppMultas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMultas.Contexto
+{
+    public class ContextoBD : DbContext
+    {
+        public ContextoBD(DbContextOptions<ContextoBD> options) : base(options)
+        {
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Multa> Multas { get; set; }
+    }
+}
